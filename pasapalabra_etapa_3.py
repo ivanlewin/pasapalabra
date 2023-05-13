b073ffe30cafce62627d6721aa7f864237753da2
VArmari03 <valenpro82@gmail.com>
2023-05-13 18:19:48 -0300

    Cambios realizados en etapa_3

--- pasapalabra_etapa_3.py
+++ pasapalabra_etapa_3.py
@@ -1,13 +1,15 @@
 import random
-from pasapalabra_etapa_2 import ordenar_filtrar_diccionario,LETRAS,sacar_tildes
+from pasapalabra_etapa_2 import ordenar_filtrar_diccionario, LETRAS, sacar_tildes
+
 
 def obtener_letras_participantes():
     '''
     Crear una lista de 10 letras aleatorias de la lista de letras permitidas para el rosco.
     '''
-    return random.sample(LETRAS,10)
+    return random.sample(LETRAS, 10)
 
-def recibir_lista_diccionario_filtrado(diccionario_filtrado,letras_participantes):
+
+def recibir_lista_diccionario_filtrado(diccionario_filtrado, letras_participantes):
 
     letras_castellano = "aábcdeéfghiíjklmnñoópqrstuúvwxyz"
     lista_palabras_participantes = []
@@ -16,17 +18,28 @@ def recibir_lista_diccionario_filtrado(diccionario_filtrado,letras_participantes
         palabras_candidatas = []
 
         for item in diccionario_filtrado:
-            palabra = item[0]  
+            palabra = item[0]
+
             palabra_sin_tildes = sacar_tildes(palabra)
 
             if palabra_sin_tildes[0] == letra:
-                palabras_candidatas.append(palabra)
+                definicion = item[1]
+                # print(definicion)
+                palabras_candidatas.append([palabra, definicion])
 
-        palabra_para_esta_letra = random.choice(palabras_candidatas) 
+        palabra_para_esta_letra = random.choice(palabras_candidatas)
+        # print(palabra_para_esta_letra)
         lista_palabras_participantes.append(palabra_para_esta_letra)
 
-    
-    return sorted(lista_palabras_participantes, key=lambda i: letras_castellano.index(i[0]))
+    # sorted(lista_palabras_participantes, key=lambda i: letras_castellano.index(i[0]))
+    return sorted(lista_palabras_participantes, key=lambda i: letras_castellano.index(i[0][0]))
+
+
+# FUNCION QUE ITERA 100 VECES PARA PRUEBAS
+for i in range(5):
+    lista_letras2 = obtener_letras_participantes()
+    diccionario_filtrado = ordenar_filtrar_diccionario()
+    print(recibir_lista_diccionario_filtrado(diccionario_filtrado, lista_letras2))
 
 """
 FUNCION QUE ITERA 100 VECES PARA PRUEBAS
